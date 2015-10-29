f277aa7c465f25d860a631cb4bc89daab557389a
koinhug@126.com <koinhug@126.com>
2015-10-29 20:17:48 +0800

    刚刚有一点点传错了

--- manage.py
+++ manage.py
@@ -6,13 +6,13 @@ __author__ = 'voidhug'
 import os
 from app import create_app, db
 from flask.ext.script import Manager, Shell
-from app.models import Admin
+from app.models import Users
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app=app, db=db, Admin=Admin)
+    return dict(app=app, db=db, Users=Users)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
